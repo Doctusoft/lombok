469adedbe416b20831702c9755d2e1f3fe14818f
gabor-farkas <gabor.farkas@doctusoft.com>
2014-04-30 14:22:15 +0200

    ObservableProperty.java
    Conflicts:
    	src/core/lombok/eclipse/handlers/EclipseHandlerUtil.java

---
+++ src/core/com/doctusoft/ObservableProperty.java
@@ -0,0 +1,15 @@
+package com.doctusoft;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.SOURCE)
+public @interface ObservableProperty {
+	/**
+	 * Readonly attributes throw an {@link UnsupportedOperationException} when written. 
+	 */
+	public boolean readonly() default false;
+}
